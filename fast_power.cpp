d6fb02c6309ec343de01ded20d00a36a97e0a640
seaurchinss <111326576+seaurchinss@users.noreply.github.com>
2022-08-19 17:49:09 +0900

    m=0の時のケースを追加

--- fast_power.cpp
+++ fast_power.cpp
@@ -2,6 +2,7 @@
 //適宜modを取る
 #define mod 1000000007
 long long power(long long n, long long m){
+    if(m==0) return 1;
     if(m==1) return n;
     if(m%2==1) return (power(n,m-1)*power(n,1))%mod;
     else return (power(n,m/2)*power(n,m/2))%mod;
